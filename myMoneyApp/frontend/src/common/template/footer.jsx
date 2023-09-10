150cc7df26dae4e7f1db17ac7bc920a98e625fb6
DaniellaMagalhaesSiqueira <daniella.siqueira80@gmail.com>
2023-09-10 14:37:21 -0300

    primeiras implementações frontend

---
+++ myMoneyApp/frontend/src/common/template/footer.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copiright &copy; 2023
+            <a href="http://cod3r.com.br" target="blank"> Cod3r</a>
+        </strong>
+    </footer>
+)
